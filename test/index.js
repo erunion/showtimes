b76cbfcf25fb2e16f229445e97d558bbef2d0e9e
Jon Ursenbach <jon@ursenba.ch>
2014-06-09 22:02:02 -0700

    Test case for a lat/long-based location.

--- test/index.js
+++ test/index.js
@@ -1,10 +1,23 @@
-var Showtimes = require('../')
+var Showtimes = require('../');
 var test = require('tap').test;
-var s = new Showtimes(94118, {
-  //date: 0
+var s = null;
+
+test('get theaters from zipcode', function (t) {
+  s = new Showtimes(94118, {
+    //date: 0
+  });
+
+  s.getTheaters(function (theaters) {
+    t.equal(theaters.length, 18);
+    t.end();
+  });
 });
 
-test('get theaters', function (t) {
+test('get theaters from lat/long', function (t) {
+  s = new Showtimes('37.7822890,-122.4637080', {
+    //date: 0
+  });
+
   s.getTheaters(function (theaters) {
     t.equal(theaters.length, 18);
     t.end();
