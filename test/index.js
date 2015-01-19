3846254c78c9c6595a349d589a891af5e185371b
Christopher Truman <cleetruman@gmail.com>
2015-01-19 14:17:11 -0800

    First attempt to resolve test failures

--- test/index.js
+++ test/index.js
@@ -5,7 +5,7 @@ var test = require('tap').test;
 var s = null;
 
 test('no movies available for a date far in the future', function (t) {
-  s = showtimes(94118, {
+  s = showtimes(90504, {
     date: 100
   });
 
@@ -16,13 +16,25 @@ test('no movies available for a date far in the future', function (t) {
 });
 
 test('get theaters from zipcode', function (t) {
-  s = showtimes(94118, {
+  s = showtimes(90504, {
     //date: 0
   });
 
   s.getTheaters(function (err, theaters) {
     t.equal(err, null);
-    t.equal(theaters.length, 18);
+    t.equal(theaters.length, 12);
+    t.end();
+  });
+});
+
+test('get theaters from foreign zipcode and theater with no phone number', function (t) {
+  s = showtimes(75201, {
+    //date: 0
+  });
+
+  s.getTheaters(function (err, theaters) {
+    t.equal(err, null);
+    t.equal(theaters.length, 2);
     t.end();
   });
 });
