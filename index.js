2077a9ca76155524775003d30909ae27c696d080
Berkay Sargın <berkaey@gmail.com>
2015-06-02 10:16:57 +0300

    default language added

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ Showtimes.prototype.getTheaters = function (cb) {
   var options = {
     url: self.baseUrl,
     qs: {
-      hl: self.lang,
+      hl: self.lang || "en",
       near: self.location,
       date: (typeof self.date !== 'undefined') ? self.date : 0,
       start: ((page - 1) * 10)
